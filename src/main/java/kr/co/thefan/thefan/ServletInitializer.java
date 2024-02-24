541675959ef22cc1c29d47976176dfbb1f5807fb
4269574 <4269574@gmail.com>
2024-02-24 14:40:34 +0900

    24.02.24 commit ( V 1.0.0 )
    uploadProject

---
+++ src/main/java/kr/co/thefan/thefan/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kr.co.thefan.thefan;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ThefanApplication.class);
+	}
+
+}
---
+++ src/main/java/kr/co/thefan/thefan/ThefanApplication.java
@@ -0,0 +1,13 @@
+package kr.co.thefan.thefan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThefanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThefanApplication.class, args);
+	}
+
+}
